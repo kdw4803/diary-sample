743e543be0a7733ce14183a46c681d7352d8f0c6
Brad <brad@Bradui-MacBook-Pro.local>
2017-03-09 16:04:56 +0900

    second commit

--- diary/ViewController.swift
+++ diary/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textArea: UITextView!
+    @IBOutlet weak var contentLabel: UILabel!
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        let text = textInput.text
+        textLabel.text = text
+        
+        let content = textArea.text
+        contentLabel.text = content
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
